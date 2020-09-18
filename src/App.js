bfd81226af4ead36028a3286092ae973a3ec8d8f
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-09-18 13:01:55 -0400

    connecting map to json data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 import "./App.css";
+import data from "./data/data.json";
+// import * as data from "./data/data.json";
 // import * as d3 from "d3";
 // import Test from "./components/Test";
 // import Map from "./components/Map";
 
 export default function App() {
+  // Viewport Settings
   const [viewport, setViewport] = useState({
     latitude: 39.7837759,
     longitude: -95.4460149,
@@ -17,13 +20,21 @@ export default function App() {
   return (
     <div>
       <ReactMapGL
+        // Map Connect & Logic
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         mapStyle="mapbox://styles/achatmajian/ckf7k6fnp0hwy19qkq0o6vfdq"
         onViewportChange={(viewport) => {
           setViewport(viewport);
         }}
-      ></ReactMapGL>
+      >
+        {data.map((infection) => (
+          // Map Connect & Logic
+          <Marker key={data.UID}>
+            <div>INFECTION</div>
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
   );
 }
