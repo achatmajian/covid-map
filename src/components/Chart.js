02f8ad000b7f6345107bf585936032df1123c9e8
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-09-20 14:08:48 -0400

    chart displaying in popup

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,15 +1,39 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
-import { Doughnut, Bar, Line } from 'react-chartjs-2';
+import { Doughnut, Bar, Line } from "react-chartjs-2";
 
-Class Chart extends Component {
-    render(){
-        return(
-    <div className="chart">
-        CHART COMPONENT
-    </div>
-        )
-    }
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+        ],
+        datasets: {
+          label: "Confirmed Cases",
+          data: [100, 150, 200, 250, 300, 350, 400, 450, 500],
+          backgroundColor: ["rgba(255, 99, 132, 0.6)"],
+        },
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div className="chart">
+        <Bar />
+      </div>
+    );
+  }
 }
 
 export default Chart;
