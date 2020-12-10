404a45c9a77022dfaf4c182e9fcb40e538faa1fb
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-12-10 11:38:37 -0500

    correct state data now appears in popup when marker is clicked

--- src/components/LoopTest.js
+++ src/components/LoopTest.js
@@ -2,20 +2,37 @@ import React from "react";
 import ToggleButtons from "./ToggleButtons.js";
 import "./LoopTestStyles.css";
 
-
 export default class LoopTest extends React.Component {
+    constructor(props) {
+        super(props);
 
-    state = {
-        loading: true,
-        usaState: null,
-    };
+        this.state = {
+            loading: true,
+            usaState: null,
+            allStates: null,
+        };
+    }
 
-    async componentDidMount() {
+    getData = async () => {
         const url = "https://api.covidtracking.com/v1/states/current.json";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ usaState: data[37], loading: false })
-        // console.log(data.results[0]);
+        return data;
+    };
+
+    async componentDidMount() {
+        const { allStates } = this.state
+        const data = await this.getData();
+        this.setState({ allStates: data });
+        this.setState({ usaState: data[this.props.stateId], loading: false });
+    }
+
+    componentDidUpdate(prevProps) {
+        const { stateId } = this.props;
+        const { allStates } = this.state
+        if (prevProps.stateId !== stateId) {
+            this.setState({ usaState: allStates[stateId], loading: false });
+        }
     }
 
     render() {
@@ -29,29 +46,58 @@ export default class LoopTest extends React.Component {
 
         return (
             <div>
-
-                <div className="data-title"><strong>State:</strong> {this.state.usaState.state}</div>
-                <div className="data-item" id="data-updated"><strong>Data Updated:</strong> {this.state.usaState.lastUpdateEt}</div>
+                <div className="data-title">
+                    <strong>State:</strong> {this.state.usaState.state}
+                </div>
+                <div className="data-item" id="data-updated">
+                    <strong>Data Updated:</strong> {this.state.usaState.lastUpdateEt}
+                </div>
                 <ToggleButtons />
                 <br />
 
-                <div className="data-title"><strong>Testing:</strong></div>
-                <div className="data-item"><strong>Positive Cases:</strong> {this.state.usaState.positive}</div>
-                <div className="data-item"><strong>Negative Cases:</strong> {this.state.usaState.negative}</div>
+                <div className="data-title">
+                    <strong>Testing Data:</strong>
+                </div>
+                <div className="data-item">
+                    <strong>Positive Cases:</strong> {this.state.usaState.positive}
+                </div>
+                <div className="data-item">
+                    <strong>Negative Cases:</strong> {this.state.usaState.negative}
+                </div>
                 <br />
 
-                <div className="data-title"><strong>Hospitalization:</strong></div>
-                <div className="data-item"><strong>Total Hospitalized:</strong> {this.state.usaState.hospitalizedCumulative}</div>
-                <div className="data-item"><strong>Currently Hospitalized:</strong> {this.state.usaState.hospitalizedCurrently}</div>
-                <div className="data-item"><strong>Currently in ICU:</strong> {this.state.usaState.inIcuCurrently}</div>
-                <div className="data-item"><strong>Currently on Ventilator:</strong> {this.state.usaState.onVentilatorCurrently}</div>
+                <div className="data-title">
+                    <strong>Hospitalization Data:</strong>
+                </div>
+                <div className="data-item">
+                    <strong>Total Hospitalized:</strong>{" "}
+                    {this.state.usaState.hospitalizedCumulative}
+                </div>
+                <div className="data-item">
+                    <strong>Currently Hospitalized:</strong>{" "}
+                    {this.state.usaState.hospitalizedCurrently}
+                </div>
+                <div className="data-item">
+                    <strong>Currently in ICU:</strong>{" "}
+                    {this.state.usaState.inIcuCurrently}
+                </div>
+                <div className="data-item">
+                    <strong>Currently on Ventilator:</strong>{" "}
+                    {this.state.usaState.onVentilatorCurrently}
+                </div>
                 <br />
 
-                <div className="data-title"><strong>Death:</strong></div>
-                <div className="data-item"><strong>Deaths:</strong> {this.state.usaState.death}</div>
-                <div className="data-item"><strong>Deaths Since Yesterday:</strong> {this.state.usaState.deathIncrease}</div>
+                <div className="data-title">
+                    <strong>Death Data:</strong>
+                </div>
+                <div className="data-item">
+                    <strong>Deaths:</strong> {this.state.usaState.death}
+                </div>
+                <div className="data-item">
+                    <strong>Deaths Since Yesterday:</strong>{" "}
+                    {this.state.usaState.deathIncrease}
+                </div>
                 <br />
-
             </div>
         );
     }
