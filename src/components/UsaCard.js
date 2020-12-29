13ab5da58232dc681a7007ea8fec1b726e7d42ae
Arek Chatmajian <arek.chatmajian@gmail.com>
2020-12-29 18:15:40 -0500

    api is console logging for usacard

--- src/components/UsaCard.js
+++ src/components/UsaCard.js
@@ -4,11 +4,39 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class UsaCard extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // Props for data rendering in pop up 
+            // loading: true,
+            country: true,
+        };
+    }
+
+    getCountryData = async () => {
+        const url = "https://api.covidtracking.com/v1/us/current.json";
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    };
+
+    async componentDidMount() {
+        const data = await this.getCountryData();
+        this.setState({ country: data });
+        console.log(data)
+    }
+
     render() {
         return (
-            <div style={{ position: "absolute", right: 0, marginTop: "580px", marginRight: "30px" }}>
+            <div style={{ position: "absolute", right: 0, marginTop: "500px", marginRight: "30px" }}>
                 <Card>
-                    <Card.Body style={{ fontSize: "10px" }}>This is some text within a card body.</Card.Body>
+
+                    <Card.Body style={{ textAlign: "center" }}>
+                        <div><strong>United States</strong></div>
+                        <div><strong>Data Last Updated: </strong></div>
+                        <div><strong>Data Quality Grade: </strong></div>
+                    </Card.Body>
                 </Card>
             </div>
         );
